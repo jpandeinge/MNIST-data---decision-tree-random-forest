a8101872a4e5ce20e94e23359fac79eb11c6000b
jpandeinge <jpberbs@gmail.com>
2021-05-25 17:41:08 +0200

    code refactoring

--- src/create_folds.py
+++ src/create_folds.py
@@ -4,7 +4,7 @@ from sklearn import  model_selection
 if __name__ == '__main__':
     
     # path of mnist train csv file
-    df_train = pd.read_csv('/home/jpandeinge/Dev/data_science/NLP/mnist/input/mnist_train.csv')
+    df_train = pd.read_csv('../input/mnist_train.csv')
     
     # create a new column of kfolds and fill it with -1
     df_train['kfold'] = -1
@@ -20,4 +20,4 @@ if __name__ == '__main__':
         df_train.loc[val_, 'kfold'] = fold
 
     # save the new csv with kfod column
-    df_train.to_csv('/home/jpandeinge/Dev/data_science/NLP/mnist/input/mnist_train_folds.csv', index=False)
+    df_train.to_csv('../input/mnist_train_folds.csv', index=False)
